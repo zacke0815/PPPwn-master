3ab28b1f6a819d02db18a85e37d170bb6eb45c59
Your Name <you@example.com>
2024-05-01 03:04:25 +0100

    run script now uses proper interface

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
+sudo apt install python3-pip python3
 sudo pip install -r requirements.txt
 make -C stage1 FW=1100 clean && make -C stage1 FW=1100
 make -C stage2 FW=1100 clean && make -C stage2 FW=1100
-sudo python3 pppwn.py --interface=eth0 --fw=1100
+sudo python3 pppwn.py --interface=ens33 --fw=1100
